8ba3206b9747d24941efc903fa3bce2d83049b2d
Haneef Ghanim <haneef@wattpad.com>
2014-07-25 13:06:21 -0400

    Fetching commands

--- server.js
+++ server.js
@@ -10,12 +10,19 @@ var app = express();
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
 var redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 redisClient.auth(redisURL.auth.split(":")[1]);
-
-// Use logger
 app.use(logfmt.requestLogger());
 
+// Main route
 app.get('/', function(req, res) {
-  res.send(req.query);
+
+	// Check for missing param
+	if (typeof req.query.text === 'undefined') {
+		res.send("Uh oh, something went wrong!");
+	}
+
+	// Initialize command array
+	var commands = req.query.text.split(" ");
+	res.send(commands);
 });
 
 app.get('/redis', function (req, res) {
@@ -25,6 +32,8 @@ app.get('/redis', function (req, res) {
 	});
 });
 
+
+// Listen - starting up server
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
