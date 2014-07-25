451f372da85dbb8452927e61314adc5bd6106a0e
Haneef Ghanim <haneef@wattpad.com>
2014-07-25 12:45:50 -0400

    Tweak redis code to work for a web server

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
+
+// Dependencies
 var express = require("express");
 var logfmt = require("logfmt");
 var redis = require('redis');
 var url = require('url');
 
+// Initialize app
 var app = express();
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
-var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-client.auth(redisURL.auth.split(":")[1]);
+var redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+redisClient.auth(redisURL.auth.split(":")[1]);
 
+// Use logger
 app.use(logfmt.requestLogger());
 
-client.set('foo', 'bar');
-client.get('foo', function (err, reply) {
-    console.log(reply.toString()); // Will print `bar`
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
 
-// app.get('/', function(req, res) {
-//   res.send('Hello World!');
-// });
+app.get('/redis', function (req, res) {
+	redisClient.set('foo', 'Redis is working!');
+	redisClient.get('foo', function (err, reply) {
+	     res.send(reply.toString());
+	});
+});
 
-// var port = Number(process.env.PORT || 5000);
-// app.listen(port, function() {
-//   console.log("Listening on " + port);
-// });
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
