3412ea3ad21ceb137186800e1de7d07e3d818bd1
Haneef Ghanim <haneef@wattpad.com>
2014-07-25 12:42:05 -0400

    Testing redis out

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var redis = require('redis');
+var url = require('url');
+
 var app = express();
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+client.set('foo', 'bar');
+client.get('foo', function (err, reply) {
+    console.log(reply.toString()); // Will print `bar`
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+// app.get('/', function(req, res) {
+//   res.send('Hello World!');
+// });
+
+// var port = Number(process.env.PORT || 5000);
+// app.listen(port, function() {
+//   console.log("Listening on " + port);
+// });
